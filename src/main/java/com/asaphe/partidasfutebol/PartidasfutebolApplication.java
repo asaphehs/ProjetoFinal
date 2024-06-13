a3f5ed1be6d41c41cd6c1e2bcb81bd5075f4e914
Asaphe Sabino <asaphhsabino@gmail.com>
2024-06-13 15:30:53 -0300

    Conectando o MySQL ao projeto

---
+++ src/main/java/com/asaphe/partidasfutebol/PartidasfutebolApplication.java
@@ -0,0 +1,13 @@
+package com.asaphe.partidasfutebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartidasfutebolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartidasfutebolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asaphe/partidasfutebol/PartidasfutebolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asaphe.partidasfutebol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartidasfutebolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
